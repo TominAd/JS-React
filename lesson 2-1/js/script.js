a0668ec334c9d8901b2ddd2ff81e440cf8fea86d
TominAd <77412394+TominAd@users.noreply.github.com>
2023-01-26 11:17:59 +0200

    lesson 3
    lesson 3

---
+++ lesson 2-1/js/script.js
@@ -0,0 +1,29 @@
+
+const vehicleBodyWidth = 5000;
+const vehicleBodyLeng = 5000;
+
+console.log("Ширина кузова автомобиля: " + vehicleBodyWidth + ", длина: " + vehicleBodyLeng);
+
+
+//Request
+//data 
+//response
+
+// snake_case
+// UPPER_SNAKE_CASE
+// Kebab-case
+// PascalCase
+
+
+let COLOR_RED = "#F00"
+
+const _apiBase = "https://gateway.marvel.com:443/vl/public/";
+// ЗДЕСЬ БУДЕТ ВАШ КЛЮЧ, ЭТОТ КЛЮЧ МОЖЕТ НЕ РАБОТАТЬ
+const _apiKey = "apikey=acdf90asdfdf8as7df09d8as7f890gs";
+
+//задание
+
+let userName = "John";
+let userNumber = 25;
+userNumber = 24;
+console.log(userName + " " + userNumber);
