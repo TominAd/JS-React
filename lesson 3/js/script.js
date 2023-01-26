e21238b92b70ff6b200df3f91bd8c2e2522ff397
TominAd <77412394+TominAd@users.noreply.github.com>
2023-01-26 14:00:22 +0200

    lesson 3
    lesson 3

--- lesson 3/js/script.js
+++ lesson 3/js/script.js
@@ -1,1 +1,30 @@
-console.log(1);
+"use sctrict";
+
+let number = 4.6;
+
+
+console.log(4/0);
+console.log(-4/0);
+console.log("string" *9);
+
+
+const persone = "Alex";
+
+const bool = false;
+
+//console.log(something);
+
+let und;
+console.log(und);
+
+const obj = {
+    name: "Jone",
+    age: 25,
+    isMarriwd: false
+};
+
+//console.log(obj.name);
+console.log(obj["name"]);
+
+let arr = ["plun.png", "orange.ipg", 6, "apple.bmp", {}, []];
+console.log(arr[1])
