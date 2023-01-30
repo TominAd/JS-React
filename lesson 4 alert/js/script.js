b321ce523f488eb4575df93f0d1589570ebe8360
TominAd <77412394+TominAd@users.noreply.github.com>
2023-01-30 16:51:05 +0200

    lesson 4
    lesson 4

---
+++ lesson 4 alert/js/script.js
@@ -0,0 +1,22 @@
+"use sctrict";
+
+//alert ("hello");
+
+//const rezult = confirm("Are you heare?: ");
+//console.log(rezult);
+
+//const answer = prompt("Вам есть 18");
+//console.log(answer);
+
+
+// const answers = [];
+// answers[0] = prompt("Как Ваше имя?:", "");
+// answers[1] = prompt("Как Ваше Фамилия?:", "");
+// answers[2] = prompt("Сколько вам лет?:", "");
+
+// document.write(answers);
+
+
+const answers = [];
+console.log(typeof(answers));
+console.log(typeof(null));
