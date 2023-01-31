2bb244131f66923df0c1b6f6795787ca280fcc6a
TominAd <77412394+TominAd@users.noreply.github.com>
2023-01-31 12:29:03 +0200

    lesson 6 operator
    lesson 6 operator

---
+++ lesson 6 operator/js/script.js
@@ -0,0 +1,34 @@
+"use strict";
+//Операторы 
+
+console.log("arr" + "- object");
+console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+
+console.log(2*4 == 8); //сравнение
+
+// const isChecked = true, //оператор И
+//         isClose = false;
+// console.log(isChecked && isClose);
+
+// const isChecked = true, //оператор или
+//         isClose = false;
+// console.log(isChecked || isClose);
+
+const isChecked = true, //оператор отрицания
+        isClose = false;
+console.log(isChecked || !isClose);
+
+// console.log(2 + 2 * 2 === 8); //порядок выполнения 
+
