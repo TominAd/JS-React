066041353bf91822f8e4347b3212edb94b9a3d92
Tomin <denis.goroshko1990@gmail.com>
2023-02-13 15:07:51 +0200

    11 dop

--- lesson 11 cycle dop/js/script.js
+++ lesson 11 cycle dop/js/script.js
@@ -40,3 +40,20 @@ first: for (let i = 0; i < 3; i++){
         }
     }
 }
+
+for (let i = 5; i <= 10; i++){
+    console.log(i);
+    
+}
+
+for (let i = 20; i >= 10; i--){
+     if (i===13) break;
+    console.log(i);
+    
+}
+
+for (let i = 2; i <= 10; i++){
+    if (i % 2 === 0){
+   console.log(i);
+    }
+}
