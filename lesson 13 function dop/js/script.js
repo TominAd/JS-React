aaec31bb78a61d03dedc960513d2645582ef5f55
Tomin <denis.goroshko1990@gmail.com>
2023-02-14 16:36:09 +0200

    lesson 13 funtion dop
    lesson 13 funtion dop

---
+++ lesson 13 function dop/js/script.js
@@ -0,0 +1,12 @@
+"use strict";
+//функции доп урок
+
+const usdCurr = 28;
+const eurCurr = 32;
+
+function convert(amount, curr){
+    console.log(curr * amount);
+}
+
+convert(500, usdCurr);
+convert(500, eurCurr);
