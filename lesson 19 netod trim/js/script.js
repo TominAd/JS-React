28af16b3f73c072c1d576022810b3aaecf29f7ae
Tomin <denis.goroshko1990@gmail.com>
2023-02-15 15:48:09 +0200

    lesson 19 metod trim
    lesson 19 metod trim

---
+++ lesson 19 netod trim/js/script.js
@@ -0,0 +1,73 @@
+
+
+'use strict';
+
+// метод trim удаляет пробельные символы с начала и конца строки
+let number0fFilms;
+
+function start () {
+    number0fFilms = +prompt ("Сколько фильмов вы уже просмотрели?", "");
+
+    while(number0fFilms == "" || number0fFilms == null || isNaN(number0fFilms)){
+        number0fFilms = +prompt ("Сколько фильмов вы уже просмотрели?", "");
+    }
+}
+start();
+
+const personalMovieDB = {
+    count: number0fFilms,
+    movies:{},
+    actors:{},
+    genres:[],
+    privat: false
+};
+
+function rememberMyFilms(){
+for (let i = 0; i < 2; i++){
+    const a = prompt ("Один из последних фильмов?", "").trim(),
+          b = prompt ("На сколько его оцените?", "");
+
+    if (a != null && b != null && a != " " && b != " " && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log("done");
+    }else{
+        console.log("error");
+        i--;
+    }
+}
+}
+
+rememberMyFilms();
+
+function detectPerconalLevel(){
+
+if (personalMovieDB.count < 10 ){
+    console.log("Посмотренно довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB < 30){
+    console.log("Вы класический зритель");
+} else if (personalMovieDB.count >= 30){
+    console.log("Вы киноман");
+} else {
+    console.log("Произошла ошибка");
+}
+}
+detectPerconalLevel();
+
+function showMyDB (hidden){
+    if(!hidden){
+        console.log(personalMovieDB);
+    }
+}
+showMyDB(personalMovieDB.privat);
+
+
+console.log(personalMovieDB);
+
+function writeYourGenres(){
+    for (let i = 1; i <= 3; i++){
+        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+        personalMovieDB.genres[i - 1] = genre;
+    }
+}
+
+writeYourGenres();
