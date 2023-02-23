48f264a0fa019087d2c56256513942c4c32eabf0
Tomin <denis.goroshko1990@gmail.com>
2023-02-23 13:21:23 +0200

    lesson 22 masiv
    `lesson 22 masiv

--- lesson 22 masive/js/script.js
+++ lesson 22 masive/js/script.js
@@ -1,43 +1,39 @@
 'use strict';
-// объект
+// масивы и псевдомасивы
 
-const options = {
-    name: "test",
-    width: 1024,
-    heigth: 1024,
-    colors: {
-        border: "black",
-        bg: "red"
-    },
-    makeTest: function(){
-        console.log("Test");
-    }
-};
+const arr = [1, 12, 33, 6, 18];
+arr.sort(compareNum);
+console.log(arr);
 
-options.makeTest();
+function compareNum(a, b) {
+    return a - b; 
+}
+compareNum();
 
-// // console.log(option.name);
 
-// // delete option.name;
-// // console.log(option);
+arr[99] = 0; 
 
-// //перебор для объектов
-// let counter = 0;
-// for(let key in options){
-//     if(typeof(options[key]) === "object"){
-//         for (let i in options[key]){
-//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
-//             counter ++;
-//         }
-//     }else {
-//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
-//         counter ++;
-//     }
-    
-// }
-// console.log(counter);
+// arr.pop(); //удаляет последний элемент массиваж
+// arr.push(10);//добавляет элемент в конец массива
+ 
+console.log(arr);
+console.log(arr.length); //свойство length состоит из последнего жлемента +1
 
-console.log(Object.keys(options).length);
+for (let i = 0; i < arr.length; i++){
+    console.log(arr[i]);
+}
 
-const {border, bg} = options.colors;
-console.log(border);
+for (let value of arr){
+    console.log(value);
+}
+
+const arr = [1, 2, 3, 6, 8];
+
+arr.forEach(function(item, i, arr) {
+    console.log(`${i}: ${item}: внутри массива ${arr}`);
+});
+
+const str = prompt("", "");
+const products = str.split(", ");
+products.sort();
+console.log(products.join('; '));
